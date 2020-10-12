236298b20923c8d62c06e8557f88d9406e00b3c4
root <root@vm228744.u72324.kvm.ubuntu.18.04.64>
2020-10-12 13:23:58 +0300

    Updates

---
+++ precache-manifest.222c3ca7b852ec92fe87ec455f61bca9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a322291a158fd7844d63fe31a7323a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efa5006e4eae845f98d5",
+    "url": "/static/css/2.d412899b.chunk.css"
+  },
+  {
+    "revision": "6108fc467757497fbb15",
+    "url": "/static/css/main.d3c4ebf8.chunk.css"
+  },
+  {
+    "revision": "efa5006e4eae845f98d5",
+    "url": "/static/js/2.63ef44b3.chunk.js"
+  },
+  {
+    "revision": "23d34883eea8063b59861e8d76b3a89d",
+    "url": "/static/js/2.63ef44b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6108fc467757497fbb15",
+    "url": "/static/js/main.669ebf41.chunk.js"
+  },
+  {
+    "revision": "53b19ab4bc6f938803ad",
+    "url": "/static/js/runtime-main.9e874143.js"
+  },
+  {
+    "revision": "23f3b7d9e33604e8efeea95b33e57d4d",
+    "url": "/static/media/greenround.23f3b7d9.png"
+  },
+  {
+    "revision": "4af5eafae822222fe37ec545ab554e4f",
+    "url": "/static/media/mask.4af5eafa.gif"
+  },
+  {
+    "revision": "d0382437d79db5761dcc7c91693ce1d7",
+    "url": "/static/media/minzdrav.d0382437.png"
+  },
+  {
+    "revision": "0290403daa4671f4457690ee109ec9cc",
+    "url": "/static/media/music.0290403d.mp4"
+  },
+  {
+    "revision": "c53b51cc12af3bc5e8abebb9908ee966",
+    "url": "/static/media/picture.c53b51cc.svg"
+  },
+  {
+    "revision": "e6a54ee92f142782761d678511bceb70",
+    "url": "/static/media/rpn.e6a54ee9.png"
+  },
+  {
+    "revision": "42992f093495f9f1b600dabf76184a94",
+    "url": "/static/media/vklogo.42992f09.png"
+  },
+  {
+    "revision": "180b2086480e90c2bcd60fa0df77681b",
+    "url": "/static/media/welcomeSound.180b2086.mp3"
+  }
+]);
